f2f05d6a5ed860dd3c9adbdd8f6a5da88fde6401
Francis Odhiambo2 <francisodhiambo2@gmail.com>
2022-06-26 22:24:04 +0300

    my to do

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form").addEventListener('submit', (e) => {
+    e.preventDefault();
+    tasks(e.target.value)
+    document.querySelector('form').reset();
+  })
 });
+
+function tasks() {
+  const input = document.getElementById("new-task-description");
+  let list = document.createElement("li");
+  let button = document.createElement("button")
+  button.addEventListener('click', removeTask)
+  button.textContent = 'DELETE';
+  list.innerHTML = `${input.value} `;
+  list.appendChild(button)
+  document.querySelector("#tasks").appendChild(list);
+}
+
+function removeTask(e) {
+  e.target.parentNode.remove();
+}
